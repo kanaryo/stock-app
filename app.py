aa67f07a97dc12308ff223455b67e91bec341519
kanaryo <kanaryo.wa.water7@gmail.com>
2026-02-01 11:01:59 +0900

    ヤフーファイナンスAPI連携の追加、リアルタイム株価取得機能追加

--- app.py
+++ app.py
@@ -2,16 +2,24 @@ import streamlit as st
 import pandas as pd
 
 from stock_data import get_dummy_stock_prices
+from stock_data import get_stock_prices
 
 st.set_page_config(page_title="Stock App", layout="wide")
 
 st.title("📈 Stock App")
 st.write("Hello Streamlit!")
 
-symbol = st.selectbox("銘柄", ["GOOGL"])
+symbol = st.selectbox(
+    "銘柄",
+    ["AAPL", "GOOGL", "MSFT", "TSLA", "AMZN"]
+)
 
-data = get_dummy_stock_prices(symbol)
-df = pd.DataFrame(data)
+period = st.selectbox(
+    "期間",
+    ["5d", "1mo", "3mo", "6mo", "1y"]
+)
+
+df = get_stock_prices(symbol, period)
 
 st.subheader(f"{symbol} の株価推移")
 st.line_chart(df.set_index("date")["price"])
--- stock_data.py
+++ stock_data.py
@@ -1,6 +1,8 @@
 from datetime import date, timedelta
+import yfinance as yf
+import pandas as pd
 
-#ダミー株価ロジック
+# ダミー株価ロジック
 def get_dummy_stock_prices(symbol: str):
     today = date.today()
     prices = [150, 152, 149, 153, 155]
@@ -12,3 +14,17 @@ def get_dummy_stock_prices(symbol: str):
         }
         for i in range(len(prices))
     ][::-1]
+
+# Yahoo Finance から株価取得ロジック
+def get_stock_prices(symbol: str, period: str = "1mo"):
+    """
+    Yahoo Finance から株価取得
+    period: 5d / 1mo / 3mo / 1y など
+    """
+
+    df = yf.download(symbol, period=period, progress=False)
+
+    df = df.reset_index()[["Date", "Close"]]
+    df.columns = ["date", "price"]
+
+    return df
