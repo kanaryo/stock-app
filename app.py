88b231b76026bba290812d0f94996f8f3d673190
kanaryo <kanaryo.wa.water7@gmail.com>
2026-04-09 20:02:32 +0900

    テストコミット

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from stock_data import get_stock_prices
 st.set_page_config(page_title="Stock App", layout="wide")
 
 st.title("📈 Stock App")
-st.write("Hello Streamlit!")
+st.write("Hello Tenda!")
 
 symbol = st.selectbox(
     "銘柄",
